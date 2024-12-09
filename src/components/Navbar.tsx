3b8cd3c3989793e7fdee18743c3c397ae7fcd0f3
Faiz <fk671384@gmail.com>
2024-12-09 04:35:50 -0800

    Updated files or added new features

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,32 +5,40 @@ import { CiSearch } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
 
-const Navbar = () => {
+// Accept className as a prop
+interface NavbarProps {
+  className?: string;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ className }) => {
   return (
-    <div className=''>
+    <div className={className}>
       {/* Centered Links */}
-      <div className='flex items-center justify-center gap-[20px] '>
+      <div className='flex items-center justify-center gap-[20px]'>
         <Link href='/'>Home</Link>
         <Link href="/shop">Shop</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
+      </div>
 
+      {/* Icons with larger sizes */}
+      <div className='flex items-center justify-end gap-[20px]'>
         <Link href='account'>
           <button className="p-2">
-            <RiAccountCircleFill className="h-[25px] w-[25px]" />
+            <RiAccountCircleFill className="h-[30px] w-[30px]" />
             <span className="sr-only">Account</span>
           </button>
         </Link>
         <button className="p-2">
-          <CiSearch className="h-[25px] w-[25px]" />
+          <CiSearch className="h-[30px] w-[30px]" />
           <span className="sr-only">Search</span>
         </button>
         <button className="p-2">
-          <CiHeart className="h-[25px] w-[25px]" />
+          <CiHeart className="h-[30px] w-[30px]" />
           <span className="sr-only">Wishlist</span>
         </button>
         <button className="p-2">
-          <CiShoppingCart className="h-[25px] w-[25px]" />
+          <CiShoppingCart className="h-[30px] w-[30px]" />
           <span className="sr-only">Cart</span>
         </button>
       </div>
